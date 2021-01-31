0f8cf02bfbc263f9b650a8ab1f2c2ef276e849f1
nambigari <varunnambigari.iit@gmail.com>
2021-01-31 10:06:05 +0530

    finished part1

--- twoPhaseMergeSort.py
+++ twoPhaseMergeSort.py
@@ -53,7 +53,7 @@ def sortSubList(subList,colOrder,colDetails,sortingOrder):
 
 def saveAsFile(currSubList,path):
     #currSubList has list of tuples
-    with open(path,'a') as filehandle:
+    with open(path,'w') as filehandle:
         for t in currSubList:
             for s in t:
                 if(s != t[len(t)-1]):
@@ -61,6 +61,16 @@ def saveAsFile(currSubList,path):
                 else:
                     filehandle.write('%s' % s)
             filehandle.write('\n')
+
+def saveAsFileFileHandler(currSubList,filehandle):
+    #currSubList has list of tuples
+    for t in currSubList:
+        for s in t:
+            if(s != t[len(t)-1]):
+                filehandle.write('%s  ' % s)
+            else:
+                filehandle.write('%s' % s)
+        filehandle.write('\n')
 
 def checkIfAllColsExist(colOrder,colDetails):
     colList = [c[0] for c in colDetails]
@@ -106,19 +116,36 @@ def getMinFromAllSublist(data,fileHandlers,phase2MaxNumberOfTuples,colDetails,co
     noOfFiles = len(fileHandlers)
     currMinValues = []
     for i in range(noOfFiles):
-        if(data[i] == [] and not fileHandlers[i].closed):
+        if(len(data[i]) == 0 and not fileHandlers[i].closed):
             data[i] = readFromSubList(fileHandlers[i],phase2MaxNumberOfTuples,colDetails)
             if(data[i] != []):
                 currMinValues.append(data[i][0])
-                data[i].pop(0)
         else:
-            currMinValues.append(data[i][0])
-            data[i].pop(0)
+            if(len(data[i])>0):
+                currMinValues.append(data[i][0])
     sortedSubList = sortSubList(currMinValues,colOrder,colDetails,sortingOrder)
     if(sortedSubList == []):
         return []
+    for i in range(len(data)):
+        if data[i]:
+            if data[i][0] == sortedSubList[0]:
+                data[i].pop(0)
     return sortedSubList[0]
 
+def removeLastNewLine(outputfileHandler):
+    outputfileHandler.seek(0,os.SEEK_END)
+    pos = outputfileHandler.tell() - 1
+    while pos > 0 and outputfileHandler.read(1) != "\n":
+        pos -= 1
+        outputfileHandler.seek(pos, os.SEEK_SET)
+    if pos > 0:
+        outputfileHandler.seek(pos, os.SEEK_SET)
+        outputfileHandler.truncate()
+    outputfileHandler.close()
+
+def printDefaultDicSizes(data):
+    for i in range(len(data)):
+        print("size "+str(i)+" : "+str(len(data[i])))
 def main():
     if(len(sys.argv) < 2):
         printError("Input not provided")
@@ -153,6 +180,8 @@ def main():
     sizeOfTuple = getTupleSize(colDetails)
     print("size of each tuple is : ",sizeOfTuple)
     noOfTuplesInSubList = int(memLimit/sizeOfTuple)
+    if(noOfTuplesInSubList == 0):
+        printError("Mem size is less than one tuple also mem : "+str(memLimit)+" tuple size "+str( sizeOfTuple))
     print("Number of tuples in a sublist : ",noOfTuplesInSubList)
     print(" ###start execution ")
     print(" ## running Phase-1")
@@ -172,6 +201,15 @@ def main():
             break
         outputTuple = parseLineRead(words,colDetails)
         currSubList.append(outputTuple)
+    
+    if currSubList:
+        sublistIndex = sublistIndex +1
+        print("sorting sublist : ",str(sublistIndex))
+        currSubList = sortSubList(currSubList,colOrder,colDetails,sortingOrder)
+        print("writing to disk : ",str(sublistIndex))
+        saveAsFile(currSubList,"subList"+str(sublistIndex)+".txt")
+        currSubList.clear()
+
     inputFile.close()
     numOfSublist = sublistIndex
     if(numOfSublist == 1):
@@ -183,17 +221,21 @@ def main():
     print("## running phase - 2")
     sizeForEachSubFile = int(memLimit/(sublistIndex+1))
     print("size available for each subfile :",str(sizeForEachSubFile))
-    if(sizeForEachSubFile < sizeOfTuple):
+    if(sizeForEachSubFile < sizeOfTuple or memLimit < (numOfSublist+1) * sizeOfTuple):
         printError("size of file is too big for two phase merge sort. Increase mem or decrease file size")
     phase2MaxNumberOfTuples = int(sizeForEachSubFile/sizeOfTuple)
 
     #dic of list of tuples
     fileHandlers = getFileHandlers(numOfSublist)
+
+    if os.path.exists(outputPath):
+        os.remove(outputPath)
+    outputfileHandler=open(outputPath, "a+")
     data = defaultdict(list)
     outputBuffer = []
     while True:
         if(len(outputBuffer) >= phase2MaxNumberOfTuples):
-            saveAsFile(outputBuffer,outputPath)
+            saveAsFileFileHandler(outputBuffer,outputfileHandler)
             outputBuffer.clear()
         currMin = getMinFromAllSublist(data,fileHandlers,phase2MaxNumberOfTuples,colDetails,colOrder,sortingOrder)
         if(currMin == []):
@@ -201,7 +243,8 @@ def main():
         else:
             outputBuffer.append(currMin)
     if(len(outputBuffer) != 0):
-        saveAsFile(outputBuffer,outputPath)
-    
+        #saveAsFile(outputBuffer,outputPath)
+        saveAsFileFileHandler(outputBuffer,outputfileHandler)
+    removeLastNewLine(outputfileHandler)
 if __name__ == '__main__':
     main()
